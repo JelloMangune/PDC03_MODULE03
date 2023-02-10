5ac3e82e063026478ead64d78a5e17fb3e1742c6
JelloMangune <enugnam.ollej@gmail.com>
2023-02-10 14:24:52 +0800

    first commit

---
+++ PDC03_MODULE03/PDC03_MODULE03/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PDC03_MODULE03.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PDC03_MODULE03.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PDC03_MODULE03/PDC03_MODULE03/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PDC03_MODULE03.Models;
+using PDC03_MODULE03.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE03.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
